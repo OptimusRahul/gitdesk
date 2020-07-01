cee67a7d4f75a62c6a201ed642dbf0db3bf3be02
OptimusRahul <rockerahul97@gmail.com>
2020-07-01 21:41:58 +0530

    removed info

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-module.exports.key = "32c11ce65c6f12c768c0";
-module.exports.secret = "1c9d13d3c2777d992103eae4b2a7f5be45acf947"
+module.exports.key = //your app key;
+module.exports.secret = //your app secret 
